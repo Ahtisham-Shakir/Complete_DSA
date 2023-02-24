1845162d6f9dd39e35a99b51e0246c93989e4a15
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-02-24 23:51:40 +0500

    Hollow Rectanlge

---
+++ Pattern Questions/Hollow_Rectangle.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int rows, col;
+    cout << "Enter no of rows: ";
+    cin >> rows;
+    cout << "Enter no of colums: ";
+    cin >> col;
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            if (i == 1 || i == rows)
+            {
+                cout << "*"
+                     << " ";
+            }
+            else if (j == 1 || j == col)
+            {
+                cout << "*"
+                     << " ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
