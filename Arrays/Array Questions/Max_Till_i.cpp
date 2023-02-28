9a37b12c9af703395bf4939338cbef6d94e96462
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-02-28 23:59:30 +0500

    Max till i

---
+++ Arrays/Array Questions/Max_Till_i.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter Size of Array ";
+    cin >> size;
+    int arr[size];
+    int mx = INT32_MIN;
+    cout << "max" << mx << endl;
+
+    cout << "Enter Array values ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        mx = max(mx, arr[i]);
+        cout << "Max till " << i << " is " << mx << endl;
+    }
+}
