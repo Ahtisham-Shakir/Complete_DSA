f880737eb335a403e8035b6788104cc3d5b15e4a
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-04-10 02:08:19 +0500

    possible paths in a maze

---
+++ Recursion/Advanced Problems/countMaze.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int countMaze(int n, int i, int j)
+{
+    if (i == n - 1 && j == n - 1)
+    {
+        return 1;
+    }
+    if (i >= n || j >= n)
+    {
+        return 0;
+    }
+
+    return (countMaze(n, i + 1, j) + countMaze(n, i, j + 1));
+}
+
+int main()
+{
+    cout << countMaze(4, 0, 0);
+
+    return 0;
+}
