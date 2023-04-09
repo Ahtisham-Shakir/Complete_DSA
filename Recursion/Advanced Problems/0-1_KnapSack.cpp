ff5e4faed06bb8e951cc138e3aee7a062199c5ed
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-04-10 03:16:34 +0500

    0-1 KnapSack problem using recursion

---
+++ Recursion/Advanced Problems/0-1_KnapSack.cpp
@@ -0,0 +1,27 @@
+/*
+put n items with given weight and value in a knapsack of capacity W to get the maximun total value in the knapsack
+*/
+#include <iostream>
+using namespace std;
+
+int knapsack(int value[], int wt[], int n, int W)
+{
+    if (n == 0 || W == 0)
+    {
+        return 0;
+    }
+
+    if (wt[n - 1] > W)
+    {
+        return knapsack(value, wt, n - 1, W);
+    }
+
+    return max(knapsack(value, wt, n - 1, W - wt[n - 1]) + value[n - 1], knapsack(value, wt, n - 1, W));
+}
+int main()
+{
+    int wt[] = {10, 20, 30};
+    int value[] = {100, 50, 150};
+    cout << knapsack(value, wt, 3, 50);
+    return 0;
+}
