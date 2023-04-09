8909f311cc4afc8e426fd3887412d08b4d3b335a
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-04-10 02:45:02 +0500

    friends pairing

---
+++ Recursion/Advanced Problems/friends_paring.cpp
@@ -0,0 +1,22 @@
+/*
+find the number of ways in which n friends can remain single or can be paired up
+*/
+#include <iostream>
+using namespace std;
+
+int friendsPairing(int n)
+{
+    if (n == 0 || n == 1 || n == 2)
+    {
+        return n;
+    }
+
+    return (friendsPairing(n - 1) + friendsPairing(n - 2) * (n - 1));
+}
+
+int main()
+{
+    cout << friendsPairing(4);
+
+    return 0;
+}
